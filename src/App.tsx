aedc94207c91cb914aa7caf58300ed439943cc30
terminalwarlord <joybiswas389@gmail.com>
2026-03-26 09:10:20 +0600

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import './App.css'
+import { IconSearch } from '@tabler/icons-react';
 
 function App() {
 
-  return <>Test</>
+  return <div>
+    <h1 className="text-4xl font-bold leading-10 flex items-center">
+      <IconSearch className='text-blue-600 w-8 h-8' />
+      <span className='pl-2 text-neutral-900'>Price</span>
+      <span className='text-blue-600'>Lens</span>
+    </h1>
+    <p className='text-neutral-600'>Compare tech prices across Bangladesh, find the best deal instantly.</p>
+  </div>
 }
 
 export default App
